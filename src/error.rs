ce2013a4b994f2c804a19717d751bd4f645e4051
Louis-Marie Baer <lm@baermail.fr>
2024-06-07 17:08:29 +0200

    feat: add doc for error enum

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,12 @@
 use thiserror::Error;
 
+/// Possible errors from the Dolibarr API
 #[derive(Error, Debug)]
 pub enum DoliApiClientError {
+    /// the product with the id provided does not exist
     #[error("product requested does not exist")]
     IdDoesNotExist,
+    /// The API response status code is an error.
     #[error(transparent)]
     ReqwestError(#[from] reqwest::Error),
 }
